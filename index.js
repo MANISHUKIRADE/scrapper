c8a224d43581fc92740532dc38e1557cf9c269ba
Manish <ukirademanish9797@gmail.com>
2021-07-24 23:12:27 +0530

    fix typo

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const fileService = require('./services/fileReader')
 
-let feature = fileService.fileReader('./data/product.csv').then(console.log).catch(console.log)
+fileService.fileReader('./data/product.csv').then(console.log).catch(console.log)

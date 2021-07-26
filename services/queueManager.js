ce28a2e569166837ee0406f825b636c5f1ff25df
Manish <ukirademanish9797@gmail.com>
2021-07-26 12:46:25 +0530

    add time indicator

--- services/queueManager.js
+++ services/queueManager.js
@@ -27,7 +27,19 @@ jobQueue.on('failed', async (job, err) => {
 })
 jobQueue.on('drained', async function () {
     let timeRequired = Date.now() - startTime
-    console.log('STOP', timeRequired)
+    console.log('Time Required', secondsToHms(timeRequired / 1000))
 })
 
+
+function secondsToHms(d) {
+    d = Number(d);
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+
+    var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+    var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+    return hDisplay + mDisplay + sDisplay;
+}
 module.exports = jobQueue;
